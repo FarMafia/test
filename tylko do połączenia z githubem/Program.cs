aace700c728ed6d4af1293b47c0ed0bdd7fae7a6
staddaleko <staddaleko@gmail.com>
2014-06-17 16:40:14 +0200

    specjalnie utworzony plik od nowa

---
+++ tylko do połączenia z githubem/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tylko_do_połączenia_z_githubem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
